764322ad0a53f839138deb4f066f62d61d5431ff
MFGangP <lovehyun95@gmail.com>
2023-07-05 17:00:37 +0900

    아두이노 서보모터 부저 연동 성공 및 JSON

--- Arduino_TEST/Arduino2_Team/Arduino2_IR_Broker_LED_Linear/Servo_Serial.py
+++ Arduino_TEST/Arduino2_Team/Arduino2_IR_Broker_LED_Linear/Servo_Serial.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import json
 #port="/dev/ttyACM0"
 # 시리얼 포트
 port="/dev/ttyACM0"
@@ -7,13 +8,33 @@ port="/dev/ttyACM0"
 serialFromARdunio = serial.Serial(port, 9600)
 
 while True:
-    user_input = input("Enter '1' or '0' : ")
+    user_input = input("Enter '1' or '-1' : ")
     if user_input == '1':
         serialFromARdunio.write(b'1')
         time.sleep(1)
-    elif user_input == '0':
-        serialFromARdunio.write(b'0')
+        try:
+            json_str = serialFromARdunio.readline().decode()
+            data = json.loads(json_str)
+
+            AD2_RCV_CGuard = data["AD2_RCV_CGuard"]
+            json_data = {
+                "AD2_RCV_CGuard" : AD2_RCV_CGuard
+            }
+        except json.JSONDecodeError:
+            print("Invaild Json Data : ", json_str)
+    elif user_input == '-1':
+        serialFromARdunio.write(b'-1')
         time.sleep(1)
+        try:
+            json_str = serialFromARdunio.readline().decode()
+            data = json.loads(json_str)
+
+            AD2_RCV_CGuard = data["AD2_RCV_CGuard"]
+            json_data = {
+                "AD2_RCV_CGuard" : AD2_RCV_CGuard
+            }
+        except json.JSONDecodeError:
+            print("Invaild Json Data : ", json_str)
     else:
         print("Invalid input. Please enter '1' or '0'.")
 
