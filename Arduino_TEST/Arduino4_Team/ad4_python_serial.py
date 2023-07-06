5b58689d1d39606301610072b56e782762338eb4
drowned <fight1999club@gmail.com>
2023-07-06 10:31:11 +0900

    Update ad4_python_serial.py

--- Arduino_TEST/Arduino4_Team/ad4_python_serial.py
+++ Arduino_TEST/Arduino4_Team/ad4_python_serial.py
@@ -5,24 +5,23 @@ port = "/dev/ttyACM0"
 ser = serial.Serial(port, 9600)    
 
 while True :
-    if ser.in_waiting > 0:
-        json_str = ser.readline().decode('utf-8').rstrip()
+    json_str = ser.readline().decode('utf-8').rstrip()
 
-        try:
-            data = json.loads(json_str)
-            AD4_NFC = data["NFC"]
-            AD4_WL_CNNT = data["WL_CNNT"]
-            AD4_WL_NCNNT = data["WL_NCNNT"]
+    try:
+        data = json.loads(json_str)
+        AD4_NFC = data["NFC"]
+        AD4_WL_CNNT = data["WL_CNNT"]
+        AD4_WL_NCNNT = data["WL_NCNNT"]
 
-            json_data = {
-                "NFC" : AD4_NFC,
-                "WL_CNNT" : AD4_WL_CNNT,
-                "WL_NCNNT" : AD4_WL_NCNNT
-            }
+        json_data = {
+            "NFC" : AD4_NFC,
+            "WL_CNNT" : AD4_WL_CNNT,
+            "WL_NCNNT" : AD4_WL_NCNNT
+        }
 
-            json_output = json.dumps(json_data)
-            print(json_output)
-        except json.JSONDecodeError:
-            print("Invalid Json Data : ", json_str)
+        json_output = json.dumps(json_data)
+        print(json_output)
+    except json.JSONDecodeError:
+        print("Invalid Json Data : ", json_str)
 
 ser.close()
