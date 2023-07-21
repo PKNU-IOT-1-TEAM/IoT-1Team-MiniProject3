7e3a4dd4a7eb7d12ae7c933bc0238df2082c05aa
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-21 18:29:17 +0900

    반응형 웹 제작 중

---
+++ Reservation_Web/PSH_Parking_Assistant_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSH_Parking_Assistant_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
