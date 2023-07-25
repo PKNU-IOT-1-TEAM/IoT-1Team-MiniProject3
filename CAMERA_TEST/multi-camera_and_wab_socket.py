d9b3d8a5b8962809f9525b1b6652660e377ba8de
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-25 22:57:18 +0900

    Update multi-camera_and_wab_socket.py

--- CAMERA_TEST/multi-camera_and_wab_socket.py
+++ CAMERA_TEST/multi-camera_and_wab_socket.py
@@ -1,131 +1,151 @@
-from ast import Try
-from picamera2 import Picamera2
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
-from threading import Condition
-from http import server
-from flask import Flask, render_template, Response
-from flask_socketio import SocketIO
-import RPi.GPIO as gp
-import time
-import os
+import io
+import logging
 import socketserver
+from http import server
+from threading import Condition
+from flask import Flask, render_template, Response
+from picamera import PiCamera
+import simplejpeg
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-PAGE = """\
+PAGE_1 = """\
 <html>
 <head>
-<title>PiCamera Streaming Demo</title>
+<title>PiCamera Streaming Demo - Camera 1</title>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.1/socket.io.js"></script>
 <script type="text/javascript">
-    var socket = io();
-    socket.on('connect', function() {
-        console.log('Connected to Flask-SocketIO');
-    });
-    socket.on('image', function(image) {
-        document.getElementById('stream').src = 'data:image/jpeg;base64,' + image;
+    var socket1 = io('/stream1');
+
+    socket1.on('image', function(image) {
+        document.getElementById('stream1').src = 'data:image/jpeg;base64,' + image;
     });
 </script>
 </head>
 <body>
-<h1>PiCamera Streaming Demo</h1>
-<img id="stream" width="640" height="480" />
+<h1>PiCamera Streaming Demo - Camera 1</h1>
+<img id="stream1" width="640" height="480" />
 </body>
 </html>
 """
 
-width = 320
-height = 240 
+PAGE_2 = """\
+<html>
+<head>
+<title>PiCamera Streaming Demo - Camera 2</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.1/socket.io.js"></script>
+<script type="text/javascript">
+    var socket2 = io('/stream2');
 
-adapter_info = {  
-    "A" : {   
-        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x04",
-        "gpio_sta":[0,0,1],
-    }, "B" : {
-        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x06",
-        "gpio_sta":[0,1,0],
-    }
-}
-
-class WorkThread(QThread):
-
-    def __init__(self):
-        super(WorkThread,self).__init__()
-        gp.setwarnings(False)
-        gp.setmode(gp.BOARD)
-        gp.setup(7, gp.OUT)
-        gp.setup(11, gp.OUT)
-        gp.setup(12, gp.OUT)
+    socket2.on('image', function(image) {
+        document.getElementById('stream2').src = 'data:image/jpeg;base64,' + image;
+    });
+</script>
+</head>
+<body>
+<h1>PiCamera Streaming Demo - Camera 2</h1>
+<img id="stream2" width="640" height="480" />
+</body>
+</html>
+"""
 
 
-    def select_channel(self,index):
-        channel_info = adapter_info.get(index)
-        if channel_info == None:
-            print("Can't get this info")
-        gpio_sta = channel_info["gpio_sta"] # gpio write
-        gp.output(7, gpio_sta[0])
-        gp.output(11, gpio_sta[1])
-        gp.output(12, gpio_sta[2])
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self, camera_id):
+        self.frame = None
+        self.condition = Condition()
+        self.camera_id = camera_id
 
-    def init_i2c(self,index):
-        channel_info = adapter_info.get(index)
-        os.system(channel_info["i2c_cmd"]) # i2c write
-
-    def run(self):
-        global picam2
-        # picam2 = Picamera2()
-        # picam2.configure( picam2.still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=1))
-
-        flag = False
-
-        for item in {"A","B"}:
-            try:
-                self.select_channel(item)
-                self.init_i2c(item)
-                time.sleep(0.5) 
-                if flag == False:
-                    flag = True
-                else :
-                    picam2.close()
-                    # time.sleep(0.5) 
-                print("init1 "+ item)
-                picam2 = Picamera2()
-                picam2.configure(picam2.create_still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=2)) 
-                picam2.start()
-                time.sleep(2)
-                picam2.capture_array(wait=False)
-                time.sleep(0.1)
-            except Exception as e:
-                print("except: "+str(e))
-
-        while True:
-            for item in {"A","B"}:
-                self.select_channel(item)
-                time.sleep(0.02)
-                try:
-                    buf = picam2.capture_array()
-                    buf = picam2.capture_array()
-
-                    if item == 'A':
-                        image_label.setPixmap(pixmap)
-                    elif item == 'B':
-                        image_label2.setPixmap(pixmap)
-                except Exception as e:
-                    print("capture_buffer: "+ str(e))
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
 
 
-# picam2 = Picamera2()
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/camera1')
+            self.end_headers()
+        elif self.path == '/camera1':
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(PAGE_1.encode('utf-8'))
+        elif self.path == '/camera2':
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(PAGE_2.encode('utf-8'))
+        elif self.path == '/stream1.mjpg':
+            self.stream_camera(1)
+        elif self.path == '/stream2.mjpg':
+            self.stream_camera(2)
+        else:
+            self.send_error(404)
+            self.end_headers()
 
-work = WorkThread()
+    def stream_camera(self, camera_id):
+        if camera_id == 1:
+            output = output1
+        elif camera_id == 2:
+            output = output2
+        else:
+            self.send_error(404)
+            self.end_headers()
+            return
 
-if __name__ == "__main__":
+        self.send_response(200)
+        self.send_header('Age', 0)
+        self.send_header('Cache-Control', 'no-cache, private')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+        self.end_headers()
 
-    work.start()
- 
-    app.exec()
-    work.quit()
-    picam2.close()
+        try:
+            while True:
+                with output.condition:
+                    output.condition.wait()
+                    frame = output.frame
+                self.wfile.write(b'--FRAME\r\n')
+                self.send_header('Content-Type', 'image/jpeg')
+                self.send_header('Content-Length', len(frame))
+                self.end_headers()
+                self.wfile.write(frame)
+                self.wfile.write(b'\r\n')
+                # Send the frame to Flask-SocketIO
+                socketio.emit('image', frame, namespace=f'/stream{camera_id}')
+        except Exception as e:
+            logging.warning(
+                'Removed streaming client %s: %s',
+                self.client_address, str(e))
 
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+if __name__ == '__main__':
+    camera1 = PiCamera(resolution=(640, 480))
+    camera2 = PiCamera(resolution=(640, 480))
+    output1 = StreamingOutput(camera_id=1)
+    output2 = StreamingOutput(camera_id=2)
+
+    camera1.start_recording(simplejpeg.jpegenc.SimJpegEnc(output1), format='mjpeg')
+    camera2.start_recording(simplejpeg.jpegenc.SimJpegEnc(output2), format='mjpeg')
+
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        socketio.init_app(app)
+        socketio.start_background_task(server.serve_forever)
+        app.run(host='0.0.0.0', port=9000, debug=False)
+    finally:
+        camera1.stop_recording()
+        camera2.stop_recording()
+        camera1.close()
+        camera2.close()
