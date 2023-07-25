42a4cb2f2cb543209a9579ee8ac5e9309cb8cb49
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-25 23:36:06 +0900

    Update multi-camera_and_wab_socket.py

--- CAMERA_TEST/multi-camera_and_wab_socket.py
+++ CAMERA_TEST/multi-camera_and_wab_socket.py
@@ -1,63 +1,84 @@
-import time
-import picamera
-import threading
-import io
+import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+import RPi.GPIO as GPIO
+from time import sleep
+import threading
+from picamera import PiCamera
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# 카메라 1 초기화
-camera1 = picamera.PiCamera()
+# Arducam에서 사용할 GPIO 핀 번호
+camera1_pin = 17
+camera2_pin = 18
+
+# 각 카메라를 위한 PiCamera 객체
+camera1 = PiCamera()
+camera2 = PiCamera()
+
+# 카메라 해상도 설정 (옵션)
 camera1.resolution = (640, 480)
-stream1 = io.BytesIO()
-
-# 카메라 2 초기화
-camera2 = picamera.PiCamera()
 camera2.resolution = (640, 480)
-stream2 = io.BytesIO()
 
-# 카메라 스레드 시작
-def camera_thread(camera, stream):
-    while True:
-        camera.capture(stream, format='jpeg')
-        stream.seek(0)
-        socketio.emit('image', stream.getvalue())
-        stream.seek(0)
-        stream.truncate()
+# 카메라 스트리밍 스레드 종료를 위한 변수
+thread_stop = False
 
-# 페이지 라우팅
+def camera_stream(camera, pin, namespace):
+    global thread_stop
+    stream_url = f'/video_stream/{pin}'
+    camera.start_preview()
+    camera.preview_fullscreen = False
+    camera.preview_window = (0, 0, 640, 480)
+
+    while not thread_stop:
+        try:
+            camera.capture(stream_url, use_video_port=True, format='jpeg')
+            socketio.emit('image_update', {'pin': pin, 'image_url': stream_url}, namespace=namespace)
+        except Exception as e:
+            print(f"Error capturing image from Camera {pin}: {e}")
+        sleep(0.1)
+
+    camera.stop_preview()
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/camera1')
-def camera1_view():
-    return render_template('camera.html')
+@socketio.on('connect', namespace='/camera1')
+def camera1_connect():
+    print('Camera 1 Client connected')
+    if not thread1.is_alive():
+        thread1.start()
 
-@app.route('/camera2')
-def camera2_view():
-    return render_template('camera.html')
+@socketio.on('connect', namespace='/camera2')
+def camera2_connect():
+    print('Camera 2 Client connected')
+    if not thread2.is_alive():
+        thread2.start()
 
-# 웹소켓 이벤트 핸들러
-@socketio.on('connect')
-def on_connect():
-    print('Client connected')
+@socketio.on('disconnect', namespace='/camera1')
+def camera1_disconnect():
+    print('Camera 1 Client disconnected')
 
-@socketio.on('disconnect')
-def on_disconnect():
-    print('Client disconnected')
-
-# 카메라 스레드 시작
-camera1_thread = threading.Thread(target=camera_thread, args=(camera1, stream1))
-camera1_thread.daemon = True
-camera1_thread.start()
-
-camera2_thread = threading.Thread(target=camera_thread, args=(camera2, stream2))
-camera2_thread.daemon = True
-camera2_thread.start()
+@socketio.on('disconnect', namespace='/camera2')
+def camera2_disconnect():
+    print('Camera 2 Client disconnected')
 
 if __name__ == '__main__':
-    # 웹소켓 서버 실행
-    socketio.run(app, host='0.0.0.0', port=5000)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(camera1_pin, GPIO.OUT)
+    GPIO.setup(camera2_pin, GPIO.OUT)
+
+    # 카메라 스트리밍을 위한 스레드 생성
+    thread1 = threading.Thread(target=camera_stream, args=(camera1, camera1_pin, '/camera1'))
+    thread2 = threading.Thread(target=camera_stream, args=(camera2, camera2_pin, '/camera2'))
+
+    try:
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    except KeyboardInterrupt:
+        thread_stop = True
+        thread1.join()
+        thread2.join()
+        GPIO.cleanup()
