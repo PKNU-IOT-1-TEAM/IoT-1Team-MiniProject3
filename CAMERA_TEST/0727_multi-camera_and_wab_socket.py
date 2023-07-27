f6fca622f7c04038640bfc5aa5155a9da59f5444
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-27 16:02:48 +0900

    test

--- CAMERA_TEST/0727_multi-camera_and_wab_socket.py
+++ CAMERA_TEST/0727_multi-camera_and_wab_socket.py
@@ -1,128 +1,39 @@
-import RPi.GPIO as gp
-import time
-import os
-from picamera2 import Picamera2
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from picamera import PiCamera
 from PIL import Image
 from io import BytesIO
-import threading
+import numpy as np
 
-width = 320
-height = 240
-
-adapter_info = {
-    "A": {
-        "i2c_cmd": "i2cset -y 10 0x70 0x00 0x04",
-        "gpio_sta": [0, 0, 1],
-    },
-    "B": {
-        "i2c_cmd": "i2cset -y 10 0x70 0x00 0x06",
-        "gpio_sta": [0, 1, 0],
-    }
-}
-
-# 각 채널에 대한 카메라 작업 처리를 위한 스레드 클래스
-class WorkThread(threading.Thread):
-    def __init__(self):
-        super(WorkThread, self).__init__()
-        gp.setwarnings(False)
-        gp.setmode(gp.BOARD)
-        gp.setup(7, gp.OUT)
-        gp.setup(11, gp.OUT)
-        gp.setup(12, gp.OUT)
-
-    def select_channel(self, index):
-        channel_info = adapter_info.get(index)
-        if channel_info is None:
-            print("해당 정보를 찾을 수 없습니다.")
-        gpio_sta = channel_info["gpio_sta"]
-        gp.output(7, gpio_sta[0])
-        gp.output(11, gpio_sta[1])
-        gp.output(12, gpio_sta[2])
-
-    def init_i2c(self, index):
-        channel_info = adapter_info.get(index)
-        os.system(channel_info["i2c_cmd"])
-
-    def run(self):
-        global picam2
-        flag = False
-
-        for item in {"A", "B"}:
-            try:
-                self.select_channel(item)
-                self.init_i2c(item)
-                time.sleep(0.5)
-
-                if flag is False:
-                    flag = True
-                else:
-                    picam2.close()
-
-                print("초기화 " + item)
-                picam2 = Picamera2()
-                picam2.configure(picam2.create_still_configuration(main={"size": (320, 240), "format": "BGR888"}, buffer_count=2))
-                picam2.start()
-                time.sleep(2)
-                picam2.capture_array(wait=False)
-                time.sleep(0.1)
-            except Exception as e:
-                print("예외: " + str(e))
-
-        while True:
-            for item in {"A", "B"}:
-                self.select_channel(item)
-                time.sleep(0.02)
-                try:
-                    buf = picam2.capture_array()
-                    buf = picam2.capture_array()
-                    img = Image.frombytes('RGB', (width, height), buf)
-                    image_bytes = BytesIO()
-                    img.save(image_bytes, format='JPEG')
-
-                    # 웹 소켓을 통해 이미지를 클라이언트로 전송
-                    socketio.emit('update_image', {'item': item, 'image': image_bytes.getvalue()}, namespace='/camera')
-                except Exception as e:
-                    print("캡처 버퍼: " + str(e))
-            time.sleep(0.1)
-
-# Flask 설정
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# 첫 번째 카메라 페이지 라우트
-@app.route('/cam1')
-def cam1():
-    return render_template('camera.html', cam_name="Camera 1")
+# 카메라 초기화
+camera = PiCamera()
+width, height = 640, 480
+camera.resolution = (width, height)
 
-# 두 번째 카메라 페이지 라우트
-@app.route('/cam2')
-def cam2():
-    return render_template('camera.html', cam_name="Camera 2")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# 웹 소켓 연결 핸들러
+def capture_and_emit():
+    while True:
+        # 이미지 캡처
+        buf = camera.capture_array()
+        img = Image.fromarray(np.uint8(buf))
+
+        # 이미지를 JPEG 형식으로 인코딩하여 웹소켓 클라이언트로 전송
+        image_bytes = BytesIO()
+        img.save(image_bytes, format='JPEG')
+        socketio.emit('update_image', {'image': image_bytes.getvalue()})
+
+# 웹소켓 연결 시 호출되는 이벤트 핸들러
 @socketio.on('connect', namespace='/camera')
 def handle_connect():
-    global picam2, work
-    work = WorkThread()
-    work.start()
-    picam2 = Picamera2()
-    picam2.configure(picam2.create_still_configuration(main={"size": (320, 240), "format": "BGR888"}, buffer_count=2))
-    picam2.start()
+    print('WebSocket client connected.')
+    # 이미지 캡처 및 웹소켓 클라이언트로의 이미지 전송을 백그라운드에서 시작
+    socketio.start_background_task(target=capture_and_emit)
 
-# 웹 소켓 연결 해제 핸들러
-@socketio.on('disconnect', namespace='/camera')
-def handle_disconnect():
-    global picam2
-    picam2.close()
-
-if __name__ == "__main__":
-    gp.setwarnings(False)
-    gp.setmode(gp.BOARD)
-    gp.setup(7, gp.OUT)
-    gp.setup(11, gp.OUT)
-    gp.setup(12, gp.OUT)
-
-    # Flask 앱을 SocketIO 지원으로 실행합니다.
+if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=9000)
