8df5515fc9cb53b47dd736ba61ec4840073ef3de
MFGangP <lovehyun95@gmail.com>
2023-07-28 00:05:15 +0900

    Update find_camera_port.py

--- CAMERA_TEST/find_camera_port.py
+++ CAMERA_TEST/find_camera_port.py
@@ -1,14 +1,7 @@
 import cv2
 
-def find_camera_port(camera_id):
-    camera = cv2.VideoCapture(camera_id)
-    if camera.isOpened():
-        print(f"카메라 {chr(ord('A') + camera_id)}의 인덱스는 {camera_id}")
-        camera.release()
-    else:
-        print(f"카메라 {chr(ord('A') + camera_id)}의 인덱스를 찾을수 없습니다")
+camera = cv2.VideoCapture(3)
 
-find_camera_port(0)
-find_camera_port(1)
-find_camera_port(2)
-find_camera_port(3)
+ret, frame = camera.read()
+
+camera.release()
