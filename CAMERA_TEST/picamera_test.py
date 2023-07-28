1ec445625e169d809f10b5f537eb68bf16314329
MFGangP <lovehyun95@gmail.com>
2023-07-28 18:09:03 +0900

    Update picamera_test.py

--- CAMERA_TEST/picamera_test.py
+++ CAMERA_TEST/picamera_test.py
@@ -3,29 +3,23 @@ from picamera2 import Picamera2 as cam2
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import threading
-import base64
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# 카메라 초기화
-picam = cam2()
-picam.preview_configuration.size = (800, 600)
-picam.preview_configuration.main.format = 'RGB888'
-picam.preview_configuration.align()
+picam2 = cam2()
+picam2.preview_configuration.main.size = (1280,720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
 
 def emit_camera_frames():
-    picam.configure(".jpg")
-    picam.start_preview()
-    picam.start()
+    picam2.configure("preview")
+    picam2.start()
 
     while True:
-        frame = picam.get_frame()  # picamera에서 현재 프레임을 가져옴
-        _, buffer = cv2.imencode(".jpg", frame)  # 프레임을 JPEG 형식으로 인코딩
-
-        # 인코딩된 프레임을 base64로 인코딩하여 클라이언트로 전송
-        encoded_frame = base64.b64encode(buffer)
-        socketio.emit("stream", encoded_frame.decode("utf-8"))
+        im= picam2.capture_array()
+        buffer = cv2.imencode(".jpg", im)
+        socketio.emit("stream", buffer)
 
 @app.route('/')
 def index():
