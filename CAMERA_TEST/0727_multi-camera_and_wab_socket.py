54fcef7c2bc36c81833672d0414296bf3e16121f
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-28 16:16:07 +0900

    test

--- CAMERA_TEST/0727_multi-camera_and_wab_socket.py
+++ CAMERA_TEST/0727_multi-camera_and_wab_socket.py
@@ -1,84 +1,53 @@
 import cv2
+import io
 import threading
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-camera_a = cv2.VideoCapture(0)  # A 카메라 포트
-if not camera_a.isOpened():
-    raise Exception("A 포트 카메라를 찾을 수 없습니다. 인덱스를 확인해주세요.")
-camera_c = cv2.VideoCapture(1)  # D 카메라 포트
-if not camera_c.isOpened():
-    raise Exception("D 포트 카메라를 찾을 수 없습니다. 인덱스를 확인해주세요.")
+# 카메라 초기화
+camera = cv2.VideoCapture(0)
+camera.set(3, 640)  # 가로 해상도 설정
+camera.set(4, 480)  # 세로 해상도 설정
 
-camera_a.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera_a.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-camera_c.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera_c.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-frame_a = None
-frame_c = None
-lock = threading.Lock()
-
-def video_stream(camera):
-    global frame_a, frame_c
-
+# 웹소켓으로 영상 전송하는 함수
+def send_video():
     while True:
-        ret, frame = camera.read()
-        if not ret:
+        success, frame = camera.read()
+        if not success:
             break
+        
+        # 영상 데이터를 바이트 스트림으로 인코딩
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame_data = buffer.tobytes()
 
-        with lock:
-            if camera == camera_a:
-                frame_a = frame.copy()
-            else:
-                frame_c = frame.copy()
+        # 웹소켓을 통해 클라이언트에게 영상 데이터 전송
+        socketio.emit('video_stream', frame_data, binary=True)
 
-thread_a = threading.Thread(target=video_stream, args=(camera_a,))
-thread_a.daemon = True
-thread_a.start()
+        # 100ms마다 영상 전송
+        socketio.sleep(0.1)
 
-thread_c = threading.Thread(target=video_stream, args=(camera_c,))
-thread_c.daemon = True
-thread_c.start()
+# 웹 페이지 렌더링
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def gen_frames(camera):
-    global frame_a, frame_c
+# 웹소켓 연결
+@socketio.on('connect')
+def on_connect():
+    print('Client connected')
+    global video_thread
+    if not video_thread.is_alive():
+        video_thread = socketio.start_background_task(send_video)
 
-    while True:
-        with lock:
-            if camera == 'a' and frame_a is not None:
-                ret, buffer = cv2.imencode('.jpg', frame_a)
-            elif camera == 'c' and frame_c is not None:
-                ret, buffer = cv2.imencode('.jpg', frame_c)
-            else:
-                continue
-
-        if not ret:
-            continue
-
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-# 각각의 웹페이지 라우트
-@app.route('/camera_a')
-def camera_a():
-    return render_template('camera_a.html')
-
-@app.route('/camera_c')
-def camera_c():
-    return render_template('camera_c.html')
-
-# 영상 스트리밍 라우트
-@app.route('/video_feed_a')
-def video_feed_a():
-    return Response(gen_frames('a'), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_feed_c')
-def video_feed_c():
-    return Response(gen_frames('c'), mimetype='multipart/x-mixed-replace; boundary=frame')
+# 웹소켓 연결 해제
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000, debug=True)
+    video_thread = threading.Thread(target=send_video)
+    video_thread.daemon = True
+    socketio.run(app, host='0.0.0.0', port=9000, debug=False)
