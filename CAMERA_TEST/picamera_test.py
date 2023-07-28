725b3e1f4974adba978fbe237da2701e03373c2d
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-07-28 17:53:19 +0900

    Update picamera_test.py

--- CAMERA_TEST/picamera_test.py
+++ CAMERA_TEST/picamera_test.py
@@ -1,51 +1,41 @@
 import cv2
 from picamera2 import Picamera2 as cam2
-from picamera2 import Preview
-from picamera2.encoders import H264Encoder
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import threading
+import base64
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 # 카메라 초기화
-# camera = cv2.VideoCapture(0)
 picam = cam2()
-picam.preview_configuration.size=(800,600)
-picam.preview_configuration.main.format='RGB888'
+picam.preview_configuration.size = (800, 600)
+picam.preview_configuration.main.format = 'RGB888'
 picam.preview_configuration.align()
 
 def emit_camera_frames():
     picam.configure(".jpg")
     picam.start_preview()
     picam.start()
-    
+
     while True:
-        frame = picam.capture_file('TEST.jpg')
-        src = cv2.imread(frame, cv2.IMREAD_COLOR)
-        if not frame:
-            break
-        
-        # 영상 데이터를 바이트 스트림으로 인코딩
-        ret, buffer = cv2.imencode('.jpg', src, [cv2.IMWRITE_WEBP_QUALITY, 100])
-        frame_data = buffer.tobytes()
+        frame = picam.get_frame()  # picamera에서 현재 프레임을 가져옴
+        _, buffer = cv2.imencode(".jpg", frame)  # 프레임을 JPEG 형식으로 인코딩
 
-        # 웹소켓을 통해 클라이언트에게 영상 데이터 전송
-        socketio.emit('video_stream', frame_data, binary=True)
+        # 인코딩된 프레임을 base64로 인코딩하여 클라이언트로 전송
+        encoded_frame = base64.b64encode(buffer)
+        socketio.emit("stream", encoded_frame.decode("utf-8"))
 
-        # 100ms마다 영상 전송
-        socketio.sleep(0.1)
-    
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # 멀티 스레딩으로 카메라 영상 송출
-    # for camera_port in camera:
-    thread = threading.Thread(target=emit_camera_frames)
-    thread.daemon = True
-    thread.start()
+    # 스트리밍 스레드를 시작
+    streaming_thread = threading.Thread(target=emit_camera_frames)
+    streaming_thread.daemon = True
+    streaming_thread.start()
 
-    socketio.run(app, host='0.0.0.0', port=9000)
+    # Flask 앱 실행
+    socketio.run(app, port=9000)
