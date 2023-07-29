aa0610d2e1231858f9e819be1da045f718b121bc
YoungHunPark0 <dltnwls0987522@gmail.com>
2023-07-29 21:20:04 +0900

    집가서 해봐

--- CAMERA_TEST/carnumberdetect.py
+++ CAMERA_TEST/carnumberdetect.py
@@ -5,7 +5,7 @@ import pytesseract
 
 # 이미지 불러오기
 plt.style.use('dark_background')
-img_ori = cv2.imread('car2.png')
+img_ori = cv2.imread('1.jpg')
 
 height, width, channel = img_ori.shape
 
@@ -204,7 +204,11 @@ for i, matched_chars in enumerate(matched_result):
         'w': int(plate_width),
         'h': int(plate_height)
     })
-    
+
+    # plt.subplot(len(matched_result), 1, i+1)
+    # plt.imshow(img_cropped, cmap='gray')
+    # plt.show()
+    # 수평은 맞춰줌  
 
 longest_idx, longest_text = -1, 0
 plate_chars = []
@@ -246,11 +250,11 @@ for i, plate_img in enumerate(plate_imgs):
     # pytesseract.pytesseract.tesseract_cmd = 'C:/DEV/Tools/'
     # chars = pytesseract.image_to_string(img_result, lang='kor', config='--psm 7 --oem 0')
     # OCR 실행
-    chars = pytesseract.image_to_string(img_result, lang='kor')
+    chars = pytesseract.image_to_string(img_result, lang='kor', config='--psm 7') # config 안에 더 추가 해야함 
     
     result_chars = ''
     has_digit = False
-    
+
     for c in chars:
         if ord('가') <= ord(c) <= ord('힣') or c.isdigit():
             if c.isdigit():
